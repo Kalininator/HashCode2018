b6bf2ffe9c12efe73a98e85b87919773e288b557
Jonathan Donaldson <jdonaldson278@gmail.com>
2018-03-01 18:16:50 +0000

    Added ID

--- Ride.py
+++ Ride.py
@@ -1,6 +1,10 @@
 class Ride:
-    def __init__(self,start,finish,earlieststart,latestfinish):
-        self.start = start
-        self.finish = finish
-        self.earlieststart = earlieststart
-        self.latestfinish = latestfinish
+	def __init__(self,ID,start,finish,earlieststart,latestfinish):
+		self.start = start
+		self.finish = finish
+		self.earlieststart = earlieststart
+		self.latestfinish = latestfinish
+		self.id = ID
+	def display(self):
+		print("ID: "+str(self.id)+" start: "+str(self.start) + " finish: "+str(self.finish)+ " Early start: "+ str(self.earlieststart)+ " Late finish: " + str(self.latestfinish))
+
