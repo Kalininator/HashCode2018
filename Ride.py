7273400fa503dac7af948bb97daa6470460a19d4
Alexander Kalinin <kalininator@gmail.com>
2018-03-01 17:56:19 +0000

    added ride class

---
+++ Ride.py
@@ -0,0 +1,6 @@
+class Ride:
+    def __init__(self,start,finish,earlieststart,latestfinish):
+        self.start = start
+        self.finish = finish
+        self.earlieststart = earlieststart
+        self.latestfinish = latestfinish
