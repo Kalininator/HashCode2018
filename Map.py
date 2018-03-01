fb4c1be8dadfd80a1e3fafece6a3c4c6996c9248
Alexander Kalinin <kalininator@gmail.com>
2018-03-01 18:35:23 +0000

    nearestCarTo - finds nearest car to location

--- Map.py
+++ Map.py
@@ -1,9 +1,29 @@
+from Vehicle import Vehicle
+
+
+def distance((startx, starty), (finishx, finishy)):
+	xdist = abs(finishx - startx)
+	ydist = abs(finishy - starty)
+	return xdist + ydist
+
+
 class Map:
-	def __init__(self, rows, columns):
+	def __init__(self, rows, columns, rides, vehicles):
 		self.rows = rows
 		self.columns = columns
+		self.rides = rides
+		self.vehicles = vehicles
 
-	def distance(self, (startx, starty), (finishx, finishy)):
-		xdist = abs(finishx - startx)
-		ydist = abs(finishy - starty)
-		return xdist + ydist
+	def nearestCarTo(self, location):
+		best_car = None
+		best_distance = None
+		for car in self.vehicles:
+			if best_car is None:
+				best_car = car
+				best_distance = distance(location, car.currentLocation)
+			else:
+				new_dist = distance(location,car.currentLocation)
+				if new_dist < best_distance:
+					best_car = car
+					best_distance = new_dist
+		return best_car
