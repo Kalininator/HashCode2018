c3af9fbe950d991785f0ed9f25a28e4d4a52d61a
Jonathan Donaldson <jdonaldson278@gmail.com>
2018-03-01 21:12:04 +0000

    Updated to proper output

--- output.py
+++ output.py
@@ -6,8 +6,14 @@ class Output:
 		self.Vehicles = VehicleList
 
 	def write(self,fileName):
+		try:
+			os.remove(fileName)
+		except OSError:
+			pass
 		file = open(fileName,"a+")
 		for vec in self.Vehicles:
+			file.write(str(len(vec.history))+ " ")
 			for i in vec.history:
 				file.write(str(i)+" ")
 			file.write("\n")
+		file.close()
