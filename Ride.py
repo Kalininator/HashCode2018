7d554eb3be7f8618f54b00a3c5d99a76532ca326
Alexander Kalinin <kalininator@gmail.com>
2018-03-01 18:20:06 +0000

    spaces to tabs >.>

--- Ride.py
+++ Ride.py
@@ -1,10 +1,11 @@
 class Ride:
-	def __init__(self,ID,start,finish,earlieststart,latestfinish):
+	def __init__(self, ID, start, finish, earlieststart, latestfinish):
 		self.start = start
 		self.finish = finish
 		self.earlieststart = earlieststart
 		self.latestfinish = latestfinish
 		self.id = ID
-	def display(self):
-		print("ID: "+str(self.id)+" start: "+str(self.start) + " finish: "+str(self.finish)+ " Early start: "+ str(self.earlieststart)+ " Late finish: " + str(self.latestfinish))
 
+	def display(self):
+		print("ID: " + str(self.id) + " start: " + str(self.start) + " finish: " + str(
+			self.finish) + " Early start: " + str(self.earlieststart) + " Late finish: " + str(self.latestfinish))
