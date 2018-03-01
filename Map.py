1d9eb4214a3d8be65f663df7257038b3282c03a3
Alexander Kalinin <kalininator@gmail.com>
2018-03-01 19:24:42 +0000

    check ride viability and nearest ride to  vehicle

--- Map.py
+++ Map.py
@@ -13,9 +13,9 @@ class Map:
 		self.columns = columns
 		self.rides = rides
 		self.vehicles = vehicles
-		#number of remaining steps
+		# number of remaining steps
 		self.steps = steps
-		#bonus points for startign a ride on time
+		# bonus points for startign a ride on time
 		self.bonus = bonus
 
 	def nearestVehicleTo(self, location):
@@ -26,7 +26,7 @@ class Map:
 				best_car = car
 				best_distance = distance(location, car.currentLocation)
 			else:
-				new_dist = distance(location,car.currentLocation)
+				new_dist = distance(location, car.currentLocation)
 				if new_dist < best_distance:
 					best_car = car
 					best_distance = new_dist
@@ -41,11 +41,31 @@ class Map:
 					best_car = car
 					best_distance = distance(location, car.currentLocation)
 				else:
-					new_dist = distance(location,car.currentLocation)
+					new_dist = distance(location, car.currentLocation)
 					if new_dist < best_distance:
 						best_car = car
 						best_distance = new_dist
 		return best_car
+
+	def isRideViable(self, ride, vehicle):
+		distToRide = distance(ride.start, vehicle.currentPosition)
+		totalDist = distToRide + ride.distance
+		return totalDist <= self.steps
+
+	def nearestRideToVehicle(self, vehicle):
+		best_ride = None
+		best_distance = None
+		for ride in self.rides:
+			if (not ride.isAssigned) and self.isRideViable(ride, vehicle):
+				if best_ride is None:
+					best_ride = ride
+					best_distance = distance(vehicle.currentPosition, ride.start)
+				else:
+					new_dist = distance(vehicle.currentPosition, ride.start)
+					if new_dist < best_distance:
+						best_ride = ride
+						best_distance = new_dist
+		return best_ride
 
 	def numFreeVehicles(self):
 		free = 0
--- Ride.py
+++ Ride.py
@@ -1,10 +1,13 @@
+from Map import distance
 class Ride:
 	def __init__(self, ID, start, finish, earlieststart, latestfinish):
 		self.start = start
 		self.finish = finish
 		self.earlieststart = earlieststart
 		self.latestfinish = latestfinish
+		self.distance = distance(start,finish)
 		self.id = ID
+		self.isAssigned = False
 
 	def display(self):
 		print("ID: " + str(self.id) + " start: " + str(self.start) + " finish: " + str(
